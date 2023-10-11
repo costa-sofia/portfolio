be18ecad8d48f8b1ab3bb283a9ca1db4823e0066
sofia.costa <costa.sofia.1408@gmail.com>
2023-10-11 14:05:30 -0300

    menu finalized

---
+++ script.js
@@ -0,0 +1,33 @@
+var texto = document.querySelector(".apresentacao");
+var sofia = document.querySelector(".sofia");
+var estudante = document.querySelector(".estudante");
+var desenvolvedora = document.querySelector(".desenvolvedora");
+
+
+sofia.addEventListener("click", function(){
+
+            texto.textContent = "sofia";
+
+    /*Queria colocar as outras opcões para mudar o nome, mas travei aqui e não sei pra onde ir kk**/
+
+
+})
+
+estudante.addEventListener("click", function(){
+
+    texto.textContent = " Sou estudante do curso técnico em Técnologia da Informaçaõ no colégio COTEMIG,fora do curso sempre fui uma apaixonada pela linguagem Java na qual desenvolvo projetos de forma autônoma.";
+
+/*Queria colocar as outras opcões para mudar o nome, mas travei aqui e não sei pra onde ir kk**/
+
+
+})
+
+desenvolvedora.addEventListener("click", function(){
+
+    texto.textContent = "desenvolvedora";
+
+/*Queria colocar as outras opcões para mudar o nome, mas travei aqui e não sei pra onde ir kk**/
+
+
+})
+
